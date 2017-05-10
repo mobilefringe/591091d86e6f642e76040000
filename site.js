8651da154e989dddf4dce4ded22ef754f76c1105
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-05-10 17:09:41 -0400

    Pacific Pearl 1.2  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Pacific Pearl",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/591091d86e6f642e76040000/image/jpeg/1493750980000/pp_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
+                }
